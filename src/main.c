e637ca051482c896ee69d29c6c9289f7332e2db1
Sholde <exshode@gmail.com>
2020-03-19 22:34:32 +0100

    fix

--- src/main.c
+++ src/main.c
@@ -1,13 +1,11 @@
-
 #include <stdlib.h>
 #include <stdio.h>
-
 
 #include "geffe.h"
 
 int main(void) {
 	
-	int n = 13;
+	int n = 45;
 	
 	const int f_size = 8;
 	char *f = malloc(sizeof(char) * f_size);
@@ -17,12 +15,12 @@ int main(void) {
 	f[3] = 1;
 	f[4] = 0;
 	f[5] = 0;
-	f[6] = 0;
-	f[7] = 1;
+	f[6] = 1;
+	f[7] = 0;
 	
 	int *k = malloc(sizeof(int) * 2);
 	k[0] = 0x0000A056;
-	k[1] = 0x564602AF;
+	k[1] = 0x56488F9F;
 	
 	char *res = generateur(f, k, n);
 	
