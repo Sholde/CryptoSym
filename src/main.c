a7704231af58b213c7499dd4917989ceac80536e
Sholde <exshode@gmail.com>
2020-03-20 13:34:43 +0100

    alea

--- src/main.c
+++ src/main.c
@@ -1,26 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "geffe.h"
 
 int main(void) {
+	time_t t;
+	t = time(NULL);
+	srand(t);
 	
 	int n = 45;
 	
 	const int f_size = 8;
 	char *f = malloc(sizeof(char) * f_size);
-	f[0] = 0;
-	f[1] = 0;
-	f[2] = 0;
-	f[3] = 1;
-	f[4] = 0;
-	f[5] = 0;
-	f[6] = 1;
-	f[7] = 0;
+	f[0] = rand()%2;
+	f[1] = rand()%2;
+	f[2] = rand()%2;
+	f[3] = rand()%2;
+	f[4] = rand()%2;
+	f[5] = rand()%2;
+	f[6] = rand()%2;
+	f[7] = rand()%2;
 	
 	int *k = malloc(sizeof(int) * 2);
-	k[0] = 0x0000A056;
-	k[1] = 0x59488F9F;
+	k[0] = rand();
+	k[1] = rand();
 	
 	char *res = generateur(f, k, n);
 	
