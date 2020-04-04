597ae19ae2d612299a5081ddc54386d3043f2a7f
Sholde <exshode@gmail.com>
2020-04-04 11:23:24 +0200

    avancement

--- src/bloc.h
+++ src/bloc.h
@@ -6,6 +6,7 @@ void step(int *l, int *r, int k);
 void bloc(int *l, int *r, int k0, int k1, int nb_tour);
 void attaque_un_tour(int *clair, int *chiffre, int *cle);
 void init_clair_chiffre(int *clair, int *chiffre, int nb_tour, int nb_texte);
+void attaque_texte(int *clair, int *chiffre, int *cle, int nb_tour, int nb_texte);
 void affiche_double(int a, char *sa, int b, char *sb);
 
 #endif 
